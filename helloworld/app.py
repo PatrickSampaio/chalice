b36645cccc9fa7d9e881d9fb0bb5409590749c9a
PatrickSampaio <patricksampaio48@gmail.com>
2017-10-08 09:31:29 -0300

    Chalice upgrade

--- helloworld/app.py
+++ helloworld/app.py
@@ -1,5 +1,8 @@
-from chalice import Chalice
+from chalice import Chalice, Response, ChaliceViewError, BadRequestError
 from chalice import UnauthorizedError
+from chalice import NotFoundError
+
+OBJECTS = {}
 
 app = Chalice(app_name='helloworld')
 app.debug = True
@@ -7,13 +10,26 @@ app.debug = True
 
 @app.route('/')
 def index():
-    return {'hello': 'world'}
+    return Response(body='hello world!',
+                    status_code=200,
+                    headers={'Content-Type': 'text/plain'})
 
 @app.route('/cities/{city}')
 def state_of_city(city):
     if city == 'portland':
-        raise
+        raise BadRequestError("error message")
     return {'state': city }
+
+@app.route('/objects/{key}', methods=['GET','PUT'])
+def myobject(key):
+    request = app.current_request
+    if request.method == 'PUT':
+        OBJECTS[key] = request.json_body
+    if request.method == 'GET':
+        try:
+            return {key: OBJECTS[key]}
+        except KeyError:
+            raise NotFoundError(key)
 
 #* BadRequestError - return a status code of 400
 #* UnauthorizedError - return a status code of 401
@@ -23,7 +39,8 @@ def state_of_city(city):
 #* TooManyRequestsError - return a status code of 429
 #* ChaliceViewError - return a status code of 500
 
-
+#Use the Response class when you want to return non-JSON content,
+# or when you want to inject custom HTTP headers to your response.
 
 
 
