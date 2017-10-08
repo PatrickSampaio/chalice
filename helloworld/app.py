f94a0bda53d3baaff393f326627a76198a623976
PatrickSampaio <patricksampaio48@gmail.com>
2017-10-08 08:44:10 -0300

    Chalice views

--- helloworld/app.py
+++ helloworld/app.py
@@ -1,29 +1,40 @@
 from chalice import Chalice
+from chalice import UnauthorizedError
 
 app = Chalice(app_name='helloworld')
+app.debug = True
 
 
 @app.route('/')
 def index():
     return {'hello': 'world'}
 
+@app.route('/cities/{city}')
+def state_of_city(city):
+    if city == 'portland':
+        raise
+    return {'state': city }
 
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
+#* BadRequestError - return a status code of 400
+#* UnauthorizedError - return a status code of 401
+#* ForbiddenError - return a status code of 403
+#* NotFoundError - return a status code of 404
+#* ConflictError - return a status code of 409
+#* TooManyRequestsError - return a status code of 429
+#* ChaliceViewError - return a status code of 500
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
